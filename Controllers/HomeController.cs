eb44623252e6de3dc5925327f567936e1c2023d5
Lucas Figueroa <lucas.200061@gmail.com>
2022-09-02 18:07:42 -0300

    c#Portfolio

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PortFolio.Models;
+namespace PortFolio.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    //ACCIONES CUANDO HACEMOS UNA PETICION HTTP A UNA RUTA ESPECIFICA
+    public IActionResult Index() // Este metodo es el que me devuelve la vista!
+    //de modo que pudedo nombrar los controladores de lo que quiero ejecutar
+    {
+        //dinamico sirve para pasar info a la view desde el controller
+        //trabaja solo en este scope;
+        /* ViewBag.Nombre = "Lucas Figueroa";
+        ViewBag.Edad = 22;
+
+       
+        //Otra Forma Fuertemente tipado...
+
+        var p = new Persona(){
+            Nombre = "Lucas Figueroa",
+            Edad = 22,
+        }; */
+
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,8 @@
+namespace PortFolio.Models
+{
+    public class Persona{
+
+        public string Nombre {get;set;}
+        public int Edad {get;set;}
+    }
+}
